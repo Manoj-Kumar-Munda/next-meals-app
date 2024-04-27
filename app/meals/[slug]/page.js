bc6364601a06d5fe518e4a6ca7e20d030cc65034
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-04-27 10:14:31 +0530

    next js meals app completed

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
+
+const MealDetailsPage = ({ params }) => {
+  const meal = getMeal(params.slug);
+
+  if(!meal){
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={`https://manoj-next-meals-app-items.s3.eu-north-1.amazonaws.com/${meal.image}`} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default MealDetailsPage;
